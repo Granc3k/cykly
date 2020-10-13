6c6dbb38fd1021b3491b3bc824a8dbaca0c83bdf
Martin Šimon <mart.simon17@gmail.com>
2020-10-13 14:58:10 +0000

    finalni verze

--- script.js
+++ script.js
@@ -22,21 +22,29 @@ function vypisPosloupnosti() {
 
 function soucetPosloupnosti() {
   let num = document.getElementById("num").valueAsNumber;
-  let s = 0;
-  let suma1 = 0;
-  for (i = 0; i < num; i++) {
-    s = s + 1;
-    suma1 = suma1 + s;
+  if(isNaN(num)){
+    alert("Není zadáno číslo Pepege Clap")
   }
-  document.getElementById("suma1").innerHTML = suma1;
+  else{
+
+    let s = 0;
+    let suma1 = 0;
+    for (i=0; i < num; i++) {
+      s = s + 1;
+      suma1 = suma1 + s;
+    }
+    document.getElementById("suma1").innerHTML = suma1;
   
   
-  let suma2 = num*(num+1)/2;
-  document.getElementById("suma2").innerHTML = suma2;
-  if (suma1==suma2){
-    document.getElementById("verdikt").innerHTML="Dle vzorce je to vpořádku :) Clap";
-  }
-  else {
-    document.getElementById("verdikt").innerHTML="Dle vzorce to není vpořádku Sadge";
+    let suma2 = num*(num+1)/2;
+    document.getElementById("suma2").innerHTML = suma2;
+  
+  
+    if (suma1!=suma2){
+      document.getElementById("verdikt").innerHTML="Dle vzorce to není vpořádku Sadge";
+    }
+    else {
+      document.getElementById("verdikt").innerHTML="Dle vzorce je to vpořádku :) Clap";
+    }
   }
 }
